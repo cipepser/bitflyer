d24ee036a75350b1b5962cfc8bf8d8f5c1452e1f
cipepser <respepic@gmail.com>
2017-05-28 21:08:10 +0900

    write usage to main.go

--- src/main.go
+++ src/main.go
@@ -1,47 +1,63 @@
 package main
 
-import (
-	"io"
-
-	"./sdk"
-)
+import "io"
 
 func test(s string, b io.Reader) {
 	return
 }
 
 const (
-	// 	// URL is a end point of bitflyer api.
+	// URL is a end point of bitflyer api.
 	URL = "https://api.bitflyer.jp"
 
 // timeout = 10
 )
 
 func main() {
-	c, _ := sdk.NewClient(URL, "user", "passwd", nil)
+	// c, _ := sdk.NewClient(URL, "user", "passwd", nil)
+
+	// 板情報の取得
 	// b := c.GetBoard("FX_BTC_JPY")
 	// fmt.Println(b)
 
+	// 証拠金の取得
 	// col := c.GetCollateral()
 	// fmt.Println(col)
 
-	// TODO: wi-fiあるところでテスト
+	// 注文を出す
 	// odr := sdk.ChildOrder{
 	// 	ProductCode:    "FX_BTC_JPY",
 	// 	ChildOrderType: "LIMIT",
 	// 	Side:           "BUY",
 	// 	Price:          200000,
 	// 	Size:           0.001,
-	// 	MinuteToExpire: 1,
+	// 	MinuteToExpire: 10,
 	// 	TimeInForce:    "GTC",
 	// }
-	//
 	// res := c.SendNewOrder(odr)
 	// fmt.Println("result: ", res)
-	//
-	// odrs := c.GetMyOrder("FX_BTC_JPY", "", "", "", "ACTIVE")
-	// fmt.Println(len(odrs))
+	// res = c.SendNewOrder(odr)
+	// fmt.Println("result: ", res)
 
-	// TODO: cancelのふたつを試す
+	// 注文状態を取得
+	// odrs := c.GetMyOrder("FX_BTC_JPY", "", "", "", "ACTIVE")
+	// for _, odr := range odrs {
+	// fmt.Println(odr.ChildOrderID)
+	// }
+
+	// 全注文をキャンセル
+	// allcan := sdk.ChildOrderAllCanceled{
+	// 	ProductCode: "FX_BTC_JPY",
+	// }
+	// res := c.CancelAllOrder(allcan)
+	// fmt.Println("result: ", res)
+
+	// 個別の注文をキャンセル
+	// can := sdk.ChildOrderCanceled{
+	// ProductCode:  "FX_BTC_JPY",
+	// ChildOrderID: odrs[0].ChildOrderID,
+	// }
+	// res := c.CancelOrder(can)
+	// fmt.Println("result: ", res)
 
 }
