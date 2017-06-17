b5b6601326d3ad09ff7905493b97fd9ab8908b6f
cipepser <respepic@gmail.com>
2017-06-17 14:11:32 +0900

    print execution size

--- src/main.go
+++ src/main.go
@@ -21,7 +21,7 @@ func main() {
 
 	for {
 		for i := 0; i < len(es); i++ {
-			fmt.Println(strconv.FormatFloat(es[len(es)-1-i].ID, 'f', 0, 64), ": ", es[len(es)-1-i].Price)
+			fmt.Println(strconv.FormatFloat(es[len(es)-1-i].ID, 'f', 0, 64), ": ", es[len(es)-1-i].Price, "円 x ", es[len(es)-1-i].Size)
 		}
 
 		time.Sleep(5 * time.Second)
